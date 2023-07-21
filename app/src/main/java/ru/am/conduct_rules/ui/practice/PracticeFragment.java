9615f78feda70a3326479aba6d48d7ab8ed218d5
MaratHMIT <hmm19792@gmail.com>
2023-07-21 17:10:37 +0300

    разворачивание правила

--- app/src/main/java/ru/am/conduct_rules/ui/practice/PracticeFragment.java
+++ app/src/main/java/ru/am/conduct_rules/ui/practice/PracticeFragment.java
@@ -1,25 +1,29 @@
 package ru.am.conduct_rules.ui.practice;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 
+import java.util.ArrayList;
+
+import ru.am.conduct_rules.Consts;
 import ru.am.conduct_rules.DataModule;
+import ru.am.conduct_rules.ProfileActivity;
 import ru.am.conduct_rules.R;
 import ru.am.conduct_rules.RuleInfo;
 import ru.am.conduct_rules.databinding.FragmentPracticeBinding;
@@ -27,16 +31,21 @@ import ru.am.conduct_rules.databinding.FragmentPracticeBinding;
 public class PracticeFragment extends Fragment {
 
     private FragmentPracticeBinding binding;
-    private LinearLayout mLinerLayoutRules;
-    private LinearLayout mLinerLayoutProgress;
+    private LinearLayout mLinerLayoutPractices;
+    private ArrayList<View> listPractice;
+    private ArrayList<View> listMainRect;
+    private ArrayList<View> listFooterRect;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentPracticeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        mLinerLayoutRules = root.findViewById(R.id.listPractices);
-        mLinerLayoutProgress = root.findViewById(R.id.listProgress);
+        mLinerLayoutPractices = root.findViewById(R.id.listPractices);
+
+        listMainRect = new ArrayList();
+        listFooterRect = new ArrayList();
+        listPractice = new ArrayList();
 
         loadPractices();
 
@@ -48,9 +57,9 @@ public class PracticeFragment extends Fragment {
         if (context == null)
             return;
 
+        int index = 0;
         int height = DataModule.convertDpToPixel(90, context);
         int paddingDP = DataModule.convertDpToPixel(4, context);
-        int sizeButtonAdd = DataModule.convertDpToPixel(36, context);
 
         Cursor cursor = DataModule.dbReader.rawQuery("SELECT r._id, r.name" +
                 " FROM rule r JOIN practice p WHERE r._id = p.rule_id GROUP BY r._id", null);
@@ -61,26 +70,43 @@ public class PracticeFragment extends Fragment {
                 rule.id = cursor.getInt(0);
                 rule.name = cursor.getString(1);
 
-                LinearLayout wrapper = new LinearLayout(context);
-                wrapper.setTag(rule.id);
-                wrapper.setOrientation(LinearLayout.VERTICAL);
-                wrapper.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout wrapperPractice = new LinearLayout(context);
+                wrapperPractice.setOrientation(LinearLayout.VERTICAL);
+                wrapperPractice.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
                         height));
-                wrapper.setBackground(context.getDrawable(R.drawable.cell_shape_orange));
+                listPractice.add(wrapperPractice);
+
+                LinearLayout wrapperPracticeHeader = new LinearLayout(context);
+                wrapperPracticeHeader.setOrientation(LinearLayout.HORIZONTAL);
+                wrapperPracticeHeader.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                        height));
+                wrapperPracticeHeader.setBackground(context.getDrawable(R.drawable.cell_shape));
+                wrapperPractice.addView(wrapperPracticeHeader);
+
+                LinearLayout wrapperPracticeFooter = new LinearLayout(context);
+                wrapperPracticeFooter.setOrientation(LinearLayout.VERTICAL);
+                wrapperPracticeFooter.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                        height));
+                wrapperPracticeFooter.setBackground(context.getDrawable(R.drawable.cell_shape));
+                wrapperPractice.addView(wrapperPracticeFooter);
+                wrapperPracticeFooter.setVisibility(View.GONE);
 
                 TextView textViewRule = new TextView(context);
                 textViewRule.setText(rule.name);
                 textViewRule.setHeight(height);
                 textViewRule.setGravity(Gravity.CENTER);
                 textViewRule.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
-                textViewRule.setLayoutParams(new FrameLayout.LayoutParams(
-                        FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+                textViewRule.setLayoutParams(new LinearLayout.LayoutParams(0, FrameLayout.LayoutParams.MATCH_PARENT, 1));
                 textViewRule.setPadding(paddingDP, 0, paddingDP, 0);
-                wrapper.addView(textViewRule);
+                textViewRule.setBackground(context.getDrawable(R.drawable.cell_shape_orange));
+                wrapperPracticeHeader.addView(textViewRule);
 
-                mLinerLayoutRules.addView(wrapper);
+                setProgressHeader(wrapperPracticeHeader, rule.id, index);
+                setProgressFooter(wrapperPracticeFooter, rule.id);
+                listFooterRect.add(wrapperPracticeFooter);
 
-                setProgress(rule.id);
+                mLinerLayoutPractices.addView(wrapperPractice);
+                index++;
 
             }
         }
@@ -88,12 +114,43 @@ public class PracticeFragment extends Fragment {
 
     }
 
-    private void setProgress(int ruleID) {
+    private void setProgressFooter(LinearLayout layout, int id) {
 
         Context context = getContext();
         if (context == null)
             return;
 
+        int size1 = DataModule.convertDpToPixel(1, context);
+        int height = DataModule.convertDpToPixel(90, context);
+
+        int h;
+        for (int i = 0; i < 3; i++) {
+            LinearLayout wrapperH = new LinearLayout(context);
+            wrapperH.setOrientation(LinearLayout.HORIZONTAL);
+            h = (i == 2) ? ((height / 3) + size1) : (height / 3);
+            wrapperH.setLayoutParams(new LinearLayout.LayoutParams(
+                    FrameLayout.LayoutParams.MATCH_PARENT, h));
+            wrapperH.setWeightSum(7);
+
+            for (int j = 0; j < 7; j++) {
+                View rect = new View(context);
+                rect.setLayoutParams(new LinearLayout.LayoutParams(0, h, 1));
+                rect.setBackground(context.getDrawable(R.drawable.cell_shape_light_gray));
+                wrapperH.addView(rect);
+            }
+
+            layout.addView(wrapperH);
+        }
+
+    }
+
+    private void setProgressHeader(LinearLayout layout, int ruleID, int index) {
+
+        Context context = getContext();
+        if (context == null)
+            return;
+
+        int widthL = DataModule.convertDpToPixel(80, context);
         int size = DataModule.convertDpToPixel(30, context);
         int size1 = DataModule.convertDpToPixel(1, context);
         int sizeI = DataModule.convertDpToPixel(26, context);
@@ -103,18 +160,11 @@ public class PracticeFragment extends Fragment {
         int marginI = DataModule.convertDpToPixel(4, context);
         int textSize = DataModule.convertDpToPixel(5, context);
 
-        LinearLayout wrapper = new LinearLayout(context);
-        wrapper.setTag(ruleID);
-        wrapper.setOrientation(LinearLayout.HORIZONTAL);
-        wrapper.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
-                height));
-        wrapper.setBackground(context.getDrawable(R.drawable.cell_shape));
-
         RelativeLayout wrapperButtonV = new RelativeLayout(context);
         wrapperButtonV.setLayoutParams(new FrameLayout.LayoutParams(widthButtons,
                 FrameLayout.LayoutParams.MATCH_PARENT));
         wrapperButtonV.setBackground(context.getDrawable(R.drawable.cell_shape));
-        wrapper.addView(wrapperButtonV);
+        layout.addView(wrapperButtonV);
 
         TextView badge = new TextView(context);
         badge.setBackground(context.getDrawable(R.drawable.bage));
@@ -135,12 +185,34 @@ public class PracticeFragment extends Fragment {
         layoutParamsButton.setMargins(marginB, marginB, marginB, marginB);
         layoutParamsButton.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
         wrapperButtonV.addView(buttonPlusMinus, layoutParamsButton);
+        buttonPlusMinus.setTag(index);
+        buttonPlusMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int i = (int) ((ImageButton) v).getTag();
+                View practice = listPractice.get(i);
+                View footer = listFooterRect.get(i);
+                View main = listMainRect.get(i);
+                LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) practice.getLayoutParams();
+
+                if (footer.getVisibility() == View.GONE) {
+                    footer.setVisibility(View.VISIBLE);
+                    main.setVisibility(View.GONE);
+                    layoutParams.height *= 2;
+                } else {
+                    footer.setVisibility(View.GONE);
+                    main.setVisibility(View.VISIBLE);
+                    layoutParams.height /= 2;
+                }
+                practice.setLayoutParams(layoutParams);
+            }
+        });
 
         LinearLayout wrapperRect = new LinearLayout(context);
         wrapperRect.setOrientation(LinearLayout.VERTICAL);
-        LinearLayout.LayoutParams layoutParamsW = new LinearLayout.LayoutParams(0, height, 1);
+        LinearLayout.LayoutParams layoutParamsW = new LinearLayout.LayoutParams( widthL, height);
         wrapperRect.setLayoutParams(layoutParamsW);
-        wrapper.addView(wrapperRect);
+        listMainRect.add(wrapperRect);
 
         int h;
         for (int i = 0; i < 3; i++) {
@@ -161,7 +233,7 @@ public class PracticeFragment extends Fragment {
             wrapperRect.addView(wrapperH);
         }
 
-        mLinerLayoutProgress.addView(wrapper);
+        layout.addView(wrapperRect);
 
     }
 
